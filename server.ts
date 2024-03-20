f3122b5ffc83a78c66f420d3aef3bbaecfedf1f3
Kabaya Taiki <taikikabaya@gmail.com>
2024-03-20 16:52:48 +0900

    serverは成功

--- server.ts
+++ server.ts
@@ -1,19 +1,30 @@
 // Expressを読み込みます。
 import express  from 'express';
+import bodyParser from 'body-parser';
+
 // Expressアプリケーションを作成します。
 const app = express();
+// bodyをjsonで受け取れるようにする
+app.use(bodyParser.json());
+// urlencodedとjsonは別々に初期化する
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
 // ポート番号を設定します。
 const PORT = 7777;
 
-// GETリクエストのルーティングを定義します。
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 // POSTリクエストのルーティングを定義します。
+// TODO: Bodyを受け取るようにする。
 app.post('/', (req, res) => {
-  res.send('Got a POST request');
+  try {
+    console.log('kabaTron: ', req.query);
+    res.send('ok');
+  } catch (e) {
+    // TODO: エラーメッセージを適切なものにする。
+    res.send(JSON.stringify(e));
+  }
 });
 
 // サーバーを起動します。
