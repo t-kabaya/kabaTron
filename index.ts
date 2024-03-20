3c0054cc5ab1bc2a23b2933e6a2a959dae777773
Kabaya Taiki <taikikabaya@gmail.com>
2024-03-20 19:52:42 +0900

    kabaTronの作成完了

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
-const axios = require('axios');
+const axios = require("axios");
 
-axios.post('http://localhost:7777',{})
-  .then(function (response) {
-    // レスポンスを処理します
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    // エラーを処理します
-    console.log(error);
-  });
+const kabaTron = async (body: any) => {
+  // 引数がオブジェクトでない場合は警告を出す
+  if (typeof body !== "object" || body === null || body === undefined) {
+    console.log("kabaTron: param must be object");
+    return;
+  }
+  axios.post("http://localhost:7777", body);
+};
+
+export default kabaTron;
