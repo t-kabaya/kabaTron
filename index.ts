a86bd70474c505636daf6e20cb4dac89a8ad27b0
Kabaya Taiki <taikikabaya@gmail.com>
2024-03-21 12:08:49 +0900

    urlを指定できるように修正

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 import axios  from "axios";
 
-const kabaTron = async (body: any) => {
+const kabaTron = async (body: any, url: string = "http://localhost:7777") => {
   // 引数がオブジェクトでない場合は警告を出す
   if (typeof body !== "object" || body === null || body === undefined) {
     console.log("kabaTron: param must be object");
     return;
   }
-  await axios.post("http://localhost:7777", body);
+  await axios.post(url, body);
 };
 
 export default kabaTron;
